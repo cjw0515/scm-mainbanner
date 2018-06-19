a18bdb40f56b42f6ba5c35659943364afa9c83b3
jw <cjw0515@naver.com>
2018-06-19 20:24:51 +0900

    초기 커밋.

---
+++ scm-mainbanner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace scm_mainbanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
