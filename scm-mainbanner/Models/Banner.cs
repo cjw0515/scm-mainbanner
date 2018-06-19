3697d3e2157bc599e3f545e1f85bf058a826afce
jw <cjw0515@naver.com>
2018-06-20 00:50:09 +0900

    배너 모델

---
+++ scm-mainbanner/Models/Banner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scm_mainbanner.Models
+{
+    public class Banner
+    {
+        public int idx { get; set; }
+        public int Id { get; set; }
+        public String bannerImageUrl { get; set; }
+        public int gubun { get; set; }
+        public int contentType { get; set; }
+        public byte linkType { get; set; }
+        public int releaseCycle { get; set; }
+        public String startDate { get; set; }
+        public String endDate{ get; set; }
+        public Char isUsing { get; set; }
+        public int priority { get; set; }
+        public String registrant { get; set; }
+        public String worker { get; set; }
+    }
+}
