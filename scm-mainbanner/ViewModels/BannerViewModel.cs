f16e8c5c1c2831519cdd25a487c8041897c142c0
jw <cjw0515@naver.com>
2018-06-20 00:50:23 +0900

    view에 넘겨줄 viewmodel

---
+++ scm-mainbanner/ViewModels/BannerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using scm_mainbanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scm_mainbanner.ViewModels
+{
+    public class BannerViewModel
+    {
+        public IEnumerable<Banner> Banners { get; set; }
+    }
+}
