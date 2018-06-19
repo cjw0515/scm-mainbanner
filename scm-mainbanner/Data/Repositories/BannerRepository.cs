6d041abbc21bc7fdb3dbfaccb74a53d5ea38c82c
jw <cjw0515@naver.com>
2018-06-20 00:51:55 +0900

    db 접근 리포지토리 추가

---
+++ scm-mainbanner/Data/Repositories/BannerRepository.cs
@@ -0,0 +1,24 @@
+﻿using scm_mainbanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scm_mainbanner.Data.Repositories
+{
+    public class BannerRepository : IBannerRepository
+    {
+        private readonly ScmContext _context;
+        public BannerRepository(ScmContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Banner> GetAllBanners()
+        {
+            var result = _context.banners.ToList();
+
+            return result;
+        }
+    }
+}
---
+++ scm-mainbanner/Data/Repositories/IBannerRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using scm_mainbanner.Models;
+
+namespace scm_mainbanner.Data.Repositories
+{
+    public interface IBannerRepository
+    {
+        IEnumerable<Banner> GetAllBanners();
+    }
+}
