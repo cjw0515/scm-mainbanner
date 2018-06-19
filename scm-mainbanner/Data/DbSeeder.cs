2e4361eadbc456735fa21d922549cfaf4bd6e1e1
jw <cjw0515@naver.com>
2018-06-20 00:49:46 +0900

    초기 데이터값 삽입을 위한 seeder클래스

---
+++ scm-mainbanner/Data/DbSeeder.cs
@@ -0,0 +1,71 @@
+﻿using scm_mainbanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scm_mainbanner.Data
+{
+    public class DbSeeder
+    {
+
+        private ScmContext _context;
+
+        public DbSeeder(ScmContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedDatabase()
+        {
+            if (!_context.banners.Any())
+            {
+                List<Banner> banners = new List<Banner>()
+                {
+                    new Banner() {
+                       bannerImageUrl = "http://sdfsfs/sdfsdf",
+                       gubun = 1,
+                       contentType = 1,
+                       linkType = 1,
+                       releaseCycle = 1,
+                       startDate = "2018-03-20",
+                       endDate = "2018-05-20",
+                       isUsing = 'Y',
+                       priority = 39,
+                       registrant = "최종원",
+                       worker = "최종원"
+                    },
+                        new Banner() {
+                       bannerImageUrl = "http://sdfsfs/sdfsdf",
+                       gubun = 1,
+                       contentType = 1,
+                       linkType = 1,
+                       releaseCycle = 1,
+                       startDate = "2018-03-20",
+                       endDate = "2018-05-20",
+                       isUsing = 'Y',
+                       priority = 39,
+                       registrant = "최종원",
+                       worker = "최종원"
+                    },
+                            new Banner() {
+                       bannerImageUrl = "http://sdfsfs/sdfsdf",
+                       gubun = 1,
+                       contentType = 1,
+                       linkType = 1,
+                       releaseCycle = 1,
+                       startDate = "2018-03-20",
+                       endDate = "2018-05-20",
+                       isUsing = 'Y',
+                       priority = 39,
+                       registrant = "최종원",
+                       worker = "최종원"
+                    }
+                };
+
+                await _context.AddRangeAsync(banners);
+                await _context.SaveChangesAsync();//삽입
+            }
+        }
+    }
+}
